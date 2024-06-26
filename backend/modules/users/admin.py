c3fedcd95f640180ce634b48e84bb113b4874b3d
Alina Mukhomediarova <alinoco.dia@gmail.com>
2024-06-26 23:02:40 +0600

    Add admin for user

--- backend/modules/users/admin.py
+++ backend/modules/users/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from rangefilter.filters import DateRangeFilter
 
-# Register your models here.
+from modules.users.models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = (
+        "email", "phone",
+        "first_name", "last_name",
+        "telegram_id",
+        "is_staff", "is_active",
+        "last_login",
+        "created_date",
+        "modified_date",
+    )
+    list_filter = (
+        "email",
+        "is_staff", "is_active",
+        ("last_login", DateRangeFilter),
+        ("created_date", DateRangeFilter),
+    )
+    fieldsets = (
+        (
+            None,
+            {"fields": ("email", "password",
+                        "username", "phone", "telegram_id",
+                        "first_name", "last_name")},
+        ),
+        ("Разрешения", {"fields": ("is_staff", "is_active")}),
+        ("Служебная информация", {"fields": ("created_date", "modified_date"),
+                                  "classes": ("grp-collapse", "grp-closed")}),
+    )
+    readonly_fields = ("created_date", "modified_date", "telegram_id")
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email",
+                       "password1", "password2",
+                       "is_staff", "is_active")
+        }),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
--- backend/modules/utils/models.py
+++ backend/modules/utils/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class TimeStampedModel(models.Model):
-    created_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+    modified_date = models.DateTimeField(verbose_name="Дата изменения", auto_now=True)
 
     class Meta:
         abstract = True
