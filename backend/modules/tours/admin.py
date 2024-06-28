74e0bf6cd0da5f0e3f87d29f58997cbffeba4d8d
Alina Mukhomediarova <alinoco.dia@gmail.com>
2024-06-28 22:27:41 +0600

    Add admin for Tour Instances

--- backend/modules/tours/admin.py
+++ backend/modules/tours/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from rangefilter.filters import DateRangeFilter
 
-from modules.tours.models import Tour
+from modules.tours.models import Tour, TourInstance
 
 
 class LocationInline(admin.TabularInline):
@@ -59,3 +59,55 @@ class TourAdmin(admin.ModelAdmin):
 
     readonly_fields = ("created_date", "modified_date")
     search_fields = ("pk", "name")
+
+
+@admin.register(TourInstance)
+class TourInstanceAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "tour",
+                    "date",
+                    "guides",
+                    "status",
+                    "maximum_people",
+                    # TODO: signed-up people
+                )
+            },
+        ),
+        (
+            "Группы в социальных сетях",
+            {
+                "fields": ("telegram_group", "whatsapp_group",),
+                "classes": ("grp-collapse", "grp-closed"),
+            },
+        ),
+        (
+            "Служебная информация",
+            {"fields": ("created_date", "modified_date"),
+             "classes": ("grp-collapse", "grp-closed")},
+        ),
+    )
+    list_display = (
+        "tour",
+        "date",
+        # TODO: get_guides
+        "status",
+        "maximum_people",
+        # TODO: signed-up people
+        "created_date",
+        "modified_date",
+    )
+
+    list_filter = (
+        "tour",
+        "date",
+        "guides",
+        "status",
+        ("created_date", DateRangeFilter)
+    )
+
+    readonly_fields = ("created_date", "modified_date")
+    search_fields = ("tour.name", "guide.name")
