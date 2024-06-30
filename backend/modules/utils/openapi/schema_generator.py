4501f9b717c02fde9562167b5297e89e790f74d4
Alina Mukhomediarova <alinoco.dia@gmail.com>
2024-07-01 02:05:05 +0600

    Enable HTTP schema for local development

--- backend/modules/utils/openapi/schema_generator.py
+++ backend/modules/utils/openapi/schema_generator.py
@@ -8,6 +8,7 @@ class _CustomSchemaGenerator(OpenAPISchemaGenerator):
     def get_schema(self, request=None, public=False):
         schema = super().get_schema(request, public)
         schema.schemes = [
+            "http",
             "https",
         ]
         return schema
